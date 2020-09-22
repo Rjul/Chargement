a3e1f391f40425b64d09dcbee4d8279bdfff850d
HUMMEL Julien <Juhummel@laposte.net>
2020-09-22 09:09:36 +0200

    init

---
+++ animGestion.js
@@ -0,0 +1,17 @@
+class ChargeeAnim{
+    constructor(){
+        var conteneurElt = document.getElementById('conteneur')
+        var boxCercleElt = document.getElementById( 'clockCharge' )
+        var vueImgElt = document.getElementById( 'logo' )
+        var boxElt = document.getElementById( 'clock' )
+        document.getElementById( 'chargerButton' ).addEventListener( "click" , () => {
+            document.getElementById('chargerButton').style.display = 'none'
+            console.log( "ok" )
+            conteneurElt.style.animation = 'both 1s chargeeStart linear'
+            setTimeout(chargerBody(),1000)
+        })
+        boxCercleElt.style.animation = 'infinite 2s rotate linear'
+        vueImgElt.style.animation = 'infinite 2s rotateReverse linear'
+    }
+}
+
---
+++ pageGestion.js
@@ -0,0 +1,25 @@
+var anim = new ChargeeAnim;
+var dataSimule = [
+    {
+        "tittle" : "Hello world!" ,
+        "article" : "Bigest transition"
+    },
+    {
+        "tittle" : "Hello world!" ,
+        "article" : "Bigest transition"
+    },
+    {
+        "tittle" : "Hello world!" ,
+        "article" : "Bigest transition"
+    },    
+]
+
+function chargerBody(){
+    console.log('ok2')
+}
+function initHeader(){
+
+}
+function initSection(){
+
+}
